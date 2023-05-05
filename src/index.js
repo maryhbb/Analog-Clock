ab6d157a8f27471a59c9af6f38c17707d5ae7e3c
Mary <mary_hbb@yahoo.com>
2023-05-05 20:34:49 +0200

    Implement updateClock function

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondEl = document.querySelector(".second")
 
+function updateClock(){
+    const currentDate = new Date();
+
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+
+    const hourDeg = (hour / 12) * 360;
+    hourEl.style.transform = `rotate(${hourDeg}deg)`
+
+    const minuteDeg = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`
+
+    const secondDeg = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`
+}
+
+setInterval(updateClock, 1000)
